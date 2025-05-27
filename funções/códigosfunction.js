cb1f309116b965f6803fb900682c70adca231075
geovana080808 <geovana.teixeira080808@gmail.com>
2025-05-27 11:27:12 -0300

    Funções aula 2°

---
+++ funções/códigosfunction.js
@@ -0,0 +1,75 @@
+console.log("Faça seu login")
+let usuarioCorreto = prompt("Digite seu usuario:")
+let senhaCorreta = prompt("Digite sua senha:")
+
+let usuario1 = "barcelona"
+let senha1 = "7410"
+
+let usuario2 = "santos"
+let senha2 = "1230"
+
+let usuario3 = "botafogo"
+let senha3 = "8522"
+
+let usuario4 = "vasco"
+let senha4 = "9630"
+
+let usuario5 = "flamengo"
+let senha5 = "1236"
+
+let usuario6 = "fluminense"
+let senha6 = "2147"
+
+let usuario7 = "real madrid"
+let senha7 = "4789"
+
+let usuario8 = "agua santa"
+let senha8 = "4569"
+
+let usuario9 = "liverpool"
+let senha9 = "0789"
+
+let usuario10 = "Corinthinas"
+let senha10 = "1910"
+
+function verificarlogin(usuario, senha){
+
+if (usuarioCorreto === usuario10 && senhaCorreta === senha10){
+console.log("Login válido!");
+}
+
+else if (usuarioCorreto === usuario1 && senhaCorreta === senha1){
+console.log("Login válido!");}
+
+else if (usuarioCorreto === usuario2 && senhaCorreta === senha2){
+console.log("Login válido!");}
+
+else if (usuarioCorreto === usuario3 && senhaCorreta === senha3){
+console.log("Login válido!");}
+
+else if (usuarioCorreto === usuario4 && senhaCorreta === senha4){
+console.log("Login válido!");}
+
+else if (usuarioCorreto === usuario5 && senhaCorreta === senha5){
+console.log("Login válido!");}
+
+else if (usuarioCorreto === usuario6 && senhaCorreta === senha6){
+console.log("Login válido!");}
+
+else if (usuarioCorreto === usuario7 && senhaCorreta === senha7){
+console.log("Login válido!");}
+
+else if (usuarioCorreto === usuario8 && senhaCorreta === senha8){
+console.log("Login válido!");}
+
+else if (usuarioCorreto === usuario9 && senhaCorreta === senha9){
+
+console.log("Login válido!");
+}
+
+else{
+console.log("Login inválido!");
+}
+}
+
+verificarlogin(usuario1,senha1,usuario2,senha2,usuario3,senha3,usuario4,senha4,usuario5,senha5,usuario6,senha6,usuario7,senha7,usuario8,senha8,usuario9,senha9, usuario10,senha10)
