a332728dc081cc02eb141a40037c2a5f280eec69
geovana080808 <geovana.teixeira080808@gmail.com>
2025-06-03 10:58:27 -0300

    exercicios

---
+++ funções/Exercicios.js
@@ -0,0 +1,120 @@
+// 1
+/*function apresentacao() {
+  console.log(&#39;Olá, Mundo!&#39;);
+}
+apresentacao();*/
+//2
+/*function mostrarNome(){
+    let nome = prompt(&quot;Qual o seu nome completo? &quot;);
+    console.log(&quot;Seu nome é &quot; + nome);
+}
+mostrarNome();*/
+//3
+/*function mostrarIdade(){
+    let idade = prompt(&quot;Qual é a sua idade? &quot;);
+    console.log(&quot;Você tem&quot; , idade , &quot;anos&quot;);
+}
+mostrarIdade();*/
+//4
+/*function somarDoisNumeros(){
+    let numero1 = Number(prompt(&quot;Escolha um número &quot;));
+    let numero2 = Number(prompt(&quot;Escolha outro numero &quot;));
+    console.log(&quot;A soma dos números é&quot;, numero1 + numero2)
+}
+somarDoisNumeros();*/
+//5
+/*function mostrarMensagem(){
+    console.log(&quot;O que você faz hoje pode melhorar todos os amanhãs.&quot;);
+}
+mostrarMensagem()*/
+//6
+/*function mostrarData(){
+    let data = prompt(&quot;Qual é a data de hoje? &quot;);
+    console.log(&quot;A data de hoje é&quot; , data);
+}
+mostrarData()*/
+//7
+/*function mostrarCidade(){
+    let cidade = prompt(&quot;Escolha uma cidade &quot;);
+    console.log(&quot;A cidade escolhida foi&quot;, cidade );
+}
+mostrarCidade()*/
+//8
+/*function saudacao(){
+    let nome = prompt(&quot;Qual o seu nome? &quot;);
+
+    console.log(&quot;Bem-vindo (a) &quot; + nome);
+}
+saudacao();*/
+// 9
+/*function subtracao(){
+    let numero1 = prompt(&quot;Escolha um número &quot;);
+    let numero2 = prompt(&quot;Escolha outro numero &quot;);
+    console.log(&quot;A subtração desses numeros é igual a &quot;, numero1 - numero2);
+}
+subtracao();*/
+//10
+/*function mostrarAnimalFavorito(){
+    let animal = prompt(&quot;Qual é o seu animal favorito? &quot;);
+    console.log(&quot;Seu animal favorito é&quot;, animal);
+}
+mostrarAnimalFavorito();*/
+//11
+/*function mensagem(){
+    console.log (&quot;Estou aprendendo JavaScript&quot;);
+}
+mensagem();*/
+//12
+/*function triplo(){
+    let numero = prompt(&quot;Escolha um número &quot;)
+    console.log(&quot;O triplo desse número é&quot;, numero * 3)
+}
+triplo()*/
+//13
+/*function informacoes(){
+    let nome = prompt(&quot;Qual é o seu nome? &quot;)
+    let idade = prompt(&quot;Quatos anos você tem? &quot;)
+    console.log(&quot;Você se chama&quot;, nome , &quot;e tem&quot;, 16, &quot;anos de idade&quot;)
+}
+informacoes()*/
+//14
+/*function numeros(){
+    console.log( &quot;1 2 3 4 5 6 7 8 9 10&quot;)
+}
+numeros()*/
+//15
+/*function exibir(){
+    console.log(&quot;Aula de funções iniciada!&quot;)
+}
+exibir()*/
+
+//16
+/*function numero(){
+ let numero = prompt(&quot;Escolha um número &quot;)
+    console.log(&quot;Você digitou o número&quot;, numero)
+}
+numero()*/
+//17
+/*function curso(){
+    let curso = prompt(&quot;Qual curso você faz? &quot;)
+    console.log(&quot;O curso é&quot;, curso)
+}
+curso()*/
+//18
+/*function chamada(){
+    let chamada = prompt(&quot;Insira o seu nome: &quot;)
+    console.log(chamada, &quot;está na chamada&quot;)
+}
+chamada()*/
+//19
+/*function cor(){
+    let cor = prompt(&quot;Qual é a sua cor favorita? &quot;)
+    console.log(&quot;Sua cor favorita é&quot;, cor)
+}
+cor()*/
+//20
+/*function país(){
+    let pais = prompt(&quot;Em que país você mora? &quot;)
+    console.log(&quot;Você mora no&quot;, pais)
+}
+país()*/
